4671e8052f189a560843e97f0f1cea3d9b808761
Ono <hoching006@gmail.com>
2024-06-06 20:47:51 -0600

    Initial Commit

---
+++ SwiftfulSouceControl/SwiftfulSouceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSouceControlApp.swift
+//  SwiftfulSouceControl
+//
+//  Created by Tupuono Ho Ching on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSouceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
